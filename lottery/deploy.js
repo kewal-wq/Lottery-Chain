204a9de60b9a1fd79bc2be012f3c6e3e8fb9a9d2
Kewal Delhiwala <kewal.delhiwala5@gmail.com>
2022-04-14 23:34:11 +0530

    Update web3 Version

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider(
   'large wasp silly birth amused laundry glance awful suit frost anxiety permit',
@@ -15,12 +15,12 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
     .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
-  console.log(interface);
+  console.log(JSON.stringify(abi));
   provider.engine.stop();
 };
 deploy();
